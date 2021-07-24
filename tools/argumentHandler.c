f4eeb4e1d5aa73c32466e060515d9c38e9284b88
Konstantinos Argyriou <rounnus@gmail.com>
2021-07-24 12:56:15 +0300

    Add missing functions.

--- tools/argumentHandler.c
+++ tools/argumentHandler.c
@@ -5,16 +5,29 @@
 #define HELP_ARG "--help"
 #define ADD_CHECK "--add-check"
 #define ADD_TARGET "--add-target"
+#define SET_CHECK_INTERVAL "--set-check-interval"
+#define SET_PARSE_INTERVAL "--set-parse-interval"
+#define SET_DEBUG_LOG "--set-debug-log"
+#define SET_DEFAULT_DIR_PATH "--set-default-dir-path"
 #define REMOVE_CHECK "--remove-check"
 #define REMOVE_TARGET "--remove-target"
 #define LIST_CHECKS "--list-checks"
 #define LIST_TARGETS "--list-targets"
+#define LIST_OPTIONS "--list-options"
 
 void help();
 
 void addCheck(char *);
 
 void addTarget(char *, char *);
+
+void setCheckInterval(char *);
+
+void setParseInterval(char *);
+
+void setDebugLog(char *);
+
+void setDefaultDirPath(char *);
 
 void removeCheck(char *);
 
@@ -23,6 +36,8 @@ void removeTarget(char *, char *);
 void listChecks();
 
 void listTargets();
+
+void listOptions();
 
 int validateArg(char*, char* , char*,char*,int, int);
 
@@ -51,11 +66,26 @@ void parse(int argc, char **argv) {
         else if (validateArg(argv[1], argv[2], argv[3], REMOVE_TARGET, 2, 15))
             removeTarget(argv[2], argv[3]);
 
+        else if (validateArg(argv[1], argv[2], NULL, SET_CHECK_INTERVAL, 1, 20))
+            setCheckInterval(argv[2]);
+
+        else if (validateArg(argv[1], argv[2], NULL, SET_PARSE_INTERVAL, 1, 20))
+            setParseInterval(argv[2]);
+
+        else if (validateArg(argv[1], argv[2], NULL, SET_DEBUG_LOG, 1, 15))
+            setDebugLog(argv[2]);
+
+        else if (validateArg(argv[1], argv[2], NULL, SET_DEFAULT_DIR_PATH, 1, 22))
+            setDefaultDirPath(argv[2]);
+
         else if (validateArg(argv[1], NULL, NULL, LIST_CHECKS, 0, 13))
             listChecks();
 
-        else if (validateArg(argv[1], NULL, NULL, LIST_TARGETS, 0, 14))
+        else if (validateArg(argv[1], NULL, NULL, LIST_OPTIONS, 0, 14))
             listTargets();
+
+        else if (validateArg(argv[1], NULL, NULL, LIST_TARGETS, 0, 14))
+            listOptions();
     }
 
 }
@@ -73,28 +103,49 @@ void help() {
 }
 
 void addCheck(char *path) {
-
+    // TODO add check.
 }
 
 void addTarget(char *ext, char *path) {
-
+    // TODO add target.
 }
 
 void removeCheck(char *path) {
-
+    // TODO remove check.
 }
 
 void removeTarget(char *ext, char *path) {
+    // TODO remove target.
+}
 
+void setCheckInterval(char *checkInterval) {
+    // TODO set check interval.
+}
+
+void setParseInterval(char *parseInterval) {
+    // TODO set parse interval.
+}
+
+void setDebugLog(char *debugLog) {
+    // TODO set debug log.
+}
+
+void setDefaultDirPath(char *defaultDirPath) {
+    // TODO set default dir path.
 }
 
 void listChecks() {
-
+    // TODO list checks.
 }
 
 void listTargets() {
-
+    // TODO list targets.
 }
+
+void listOptions() {
+    // TODO list options.
+}
+
 
 int validateArg(char* arg_1,
                  char* arg_2,
