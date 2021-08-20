e1d1e40735510b2fc807bf9dc186072fdfcaea26
Konstantinos Argyriou <rounnus@gmail.com>
2021-08-20 07:49:02 +0300

    Make more efficient handle of arguments,

--- tools/argumentHandler.h
+++ tools/argumentHandler.h
@@ -1,9 +1,13 @@
 #ifndef FILE_SORTER_TERMINAL_TOOL_ARGUMENTHANDLER_H
 #define FILE_SORTER_TERMINAL_TOOL_ARGUMENTHANDLER_H 1
 
-extern void parse(int argc, char** argv);
+#include "argumentParser.h"
 
+extern void help();
 
+extern void setter(struct commandP commandP, char *newValue);
+
+extern void listContent(struct commandP commandP);
 
 #endif
 
