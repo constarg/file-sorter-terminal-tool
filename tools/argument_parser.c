07578c33276e4d62d1a9a11cc281b6ae3b2651f4
Konstantinos Argyriou <constarg@pm.me>
2021-10-13 01:15:27 +0300

    Clean up the previous approach.

--- tools/argument_parser.c
+++ tools/argument_parser.c
@@ -58,15 +58,26 @@ const struct command_p commands[NUMBER_OF_COMMANDS] = {
         {REMOVE_TARGET,        ADD_REMOVE_TYPE, 0, TARGET_ID,     TARGET_ID_D,  IS_NOT_ADD,     ARGS_1},
         {SET_CHECK_INTERVAL,   SETTER_TYPE,     1, C_INTERVAL_OP, NULL,         IS_INTEGER,     ARGS_1},
         {SET_PARSE_INTERVAL,   SETTER_TYPE,     2, P_INTERVAL_OP, NULL,         IS_INTEGER,     ARGS_1},
-        {SET_DEBUG_LOG,        SETTER_TYPE,     3, D_LOG_OP,      NULL,  	IS_INTEGER,     ARGS_1},
-        {SET_DEFAULT_DIR_PATH, SETTER_TYPE,     4, D_PATH_OP,     NULL, 	IS_NOT_INTEGER, ARGS_1},
-        {SET_ENABLE_DEF_DIR,   SETTER_TYPE,     5, D_ENABL_OP,    NULL,         IS_INTEGER, ARGS_1},
+        {SET_DEBUG_LOG,        SETTER_TYPE,     3, D_LOG_OP,      NULL,  	    IS_INTEGER,     ARGS_1},
+        {SET_DEFAULT_DIR_PATH, SETTER_TYPE,     4, D_PATH_OP,     NULL, 	    IS_NOT_INTEGER, ARGS_1},
+        {SET_ENABLE_DEF_DIR,   SETTER_TYPE,     5, D_ENABL_OP,    NULL,         IS_INTEGER,     ARGS_1},
         {LIST_CHECKS,          LIST_TYPE,       0, CHECK_ID,      CHECK_ID_D,   IS_NOT_INTEGER, ARGS_0},
         {LIST_TARGETS,         LIST_TYPE,       0, TARGET_ID,     TARGET_ID_D,  IS_NOT_INTEGER, ARGS_0},
         {LIST_OPTIONS,         LIST_TYPE,       0, C_INTERVAL_OP, CHECK_ID,     IS_NOT_INTEGER, ARGS_0}
 };
 
-int find_command(char *, int *);
+
+static inline int find_command(char *name, int *resIndex) {
+    // Find the command.
+    for (int command = 0; command < NUMBER_OF_COMMANDS; command++) {
+        if (strcmp(commands[command].name, name) == 0) {
+            *resIndex = command;
+            return 0;
+        }
+    }
+
+    return -1;
+}
 
 void parse_args(int argc, char **argv) {
     if (argc == 1) help();
@@ -84,14 +95,3 @@ void parse_args(int argc, char **argv) {
     else if (strcmp(commands[command_index].type, ADD_REMOVE_TYPE) == 0) add_or_remove(commands[command_index], argv);
 }
 
-int find_command(char *name, int *resIndex) {
-    // Find the command.
-    for (int command = 0; command < NUMBER_OF_COMMANDS; command++) {
-        if (strcmp(commands[command].name, name) == 0) {
-            *resIndex = command;
-            return 0;
-        }
-    }
-
-    return -1;
-}
---
+++ tools/include/mem/mem.h
@@ -0,0 +1,18 @@
+#ifndef FILE_SORTER_TERMINAL_TOOL_MEM_H
+#define FILE_SORTER_TERMINAL_TOOL_MEM_H
+
+#include <stdlib.h>
+
+#define OUT_OF_MEMORY_ERROR -1
+
+#define ALLOCATE_MEMORY(VAR, SIZE, SIZEOF_TYPE) do {                \
+        VAR = calloc(SIZE, SIZEOF_TYPE);                            \
+        if (VAR == -1) exit(OUT_OF_MEMORY_ERROR);                   \
+    } while(0)
+
+#define REALLOCATE_MEMORY(VAR, NEW_SIZE, SIZEOF_TYPE) do {          \
+        VAR = realloc(VAR, NEW_SIZE * SIZEOF_TYPE);                 \
+        if (VAR == -1) exit(OUT_OF_MEMORY_ERROR);                   \
+    } while(0)
+
+#endif
