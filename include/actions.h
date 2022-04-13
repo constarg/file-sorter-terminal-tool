27a18124b31d2bf95f70cd7fda29269a332d9dfb
Constantinos Argyriou <constarg@pm.me>
2022-04-13 20:27:38 +0300

    add a few comments

--- include/actions.h
+++ include/actions.h
@@ -4,19 +4,58 @@
 #include <tool_builder/tool_builder.h>
 
 // option manipulation commands.
+/**
+ * Change the value of the check interval option
+ * in config file.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void set_check_interval(const struct tool_builder_args *args);
 
+/**
+ * Change the value of the parse interval option
+ * in config file.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void set_parse_interval(const struct tool_builder_args *args);
 
+/**
+ * Change the default dir path, where the
+ * files sented when no rule has been set.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void set_default_dir_path(const struct tool_builder_args *args);
 
+/**
+ * Enable or disable the default dir
+ * feature. This works by changing
+ * the value of the coresponded value
+ * in config file.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void set_enable_default_dir(const struct tool_builder_args *args);
 
+/**
+ * Enable or disable the debug log
+ * feature. This works by changing
+ * the value of the coresponded value
+ * in config file.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void set_debug_log(const struct tool_builder_args *args);
 
 // list manipulation commands.
+/**
+ * Add a new path on the check list that the core
+ * will scan for files.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void add_check(const struct tool_builder_args *args);
 
+/**
+ * Add a new rule on the target list that the core
+ * checks in order to orginize the files.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void add_target(const struct tool_builder_args *args);
 
 extern void add_exclude(const struct tool_builder_args *args);
--- include/config.h
+++ include/config.h
@@ -1,8 +1,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/**
+ * Write the config file contents.
+ * @param src The buffer that contains the changed content.
+ * @return 0 on success or -1 on error.
+ */
 extern int write_config(const char *src);
 
+/**
+ * Reads the config file contents and
+ * store them in a buffer.
+ * @param dst The destination buffer.
+ * @return 0 on success or -1 on error.
+ */
 extern int read_config(char *dst);
 
 #endif
