8ebe6e5c3f8a23985f0d59b5b4b78fccbe4e0d52
Constantinos Argyriou <constarg@pm.me>
2022-04-13 20:10:37 +0300

    Configure config interface.

--- src/actions.c
+++ src/actions.c
@@ -1,77 +1,77 @@
 #include <actions.h>
 
 
-extern void set_check_interval(const struct tool_builder_args *args)
+void set_check_interval(const struct tool_builder_args *args)
 {
 
 }
 
-extern void set_parse_interval(const struct tool_builder_args *args)
+void set_parse_interval(const struct tool_builder_args *args)
 {
 
 }
 
-extern void set_default_dir_path(const struct tool_builder_args *args)
+void set_default_dir_path(const struct tool_builder_args *args)
 {
 
 }
 
-extern void set_enable_default_dir(const struct tool_builder_args *args)
+void set_enable_default_dir(const struct tool_builder_args *args)
 {
 
 }
 
-extern void set_debug_log(const struct tool_builder_args *args)
+void set_debug_log(const struct tool_builder_args *args)
 {
 
 }
 
-extern void add_check(const struct tool_builder_args *args)
+void add_check(const struct tool_builder_args *args)
 {
 
 }
 
-extern void add_target(const struct tool_builder_args *args)
+void add_target(const struct tool_builder_args *args)
 {
 
 }
 
-extern void add_exclude(const struct tool_builder_args *args)
+void add_exclude(const struct tool_builder_args *args)
 {
 
 }
 
-extern void remove_check(const struct tool_builder_args *args)
+void remove_check(const struct tool_builder_args *args)
 {
 
 }
 
-extern void remove_target(const struct tool_builder_args *args)
+void remove_target(const struct tool_builder_args *args)
 {
 
 }
 
-extern void remove_exclude(const struct tool_builder_args *args)
+void remove_exclude(const struct tool_builder_args *args)
 {
 
 }
 
-extern void list_options(const struct tool_builder_args *args)
+void list_options(const struct tool_builder_args *args)
 {
 
 }
 
-extern void list_checks(const struct tool_builder_args *args)
+void list_checks(const struct tool_builder_args *args)
 {
 
 }
 
-extern void list_targets(const struct tool_builder_args *args)
+void list_targets(const struct tool_builder_args *args)
 {
 
 }
 
-extern void list_excludes(const struct tool_builder_args *args)
+void list_excludes(const struct tool_builder_args *args)
 {
 
 }
