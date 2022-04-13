1e10ade3d556647e2ac4fb45cc9e6263572a9a77
Constantinos Argyriou <rounnus@gmail.com>
2022-04-13 20:10:37 +0300

    Configure config interface.

---
+++ include/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern int write_config(const char *src);
+
+extern int read_config(char *dst);
+
+#endif
---
+++ src/config.c
@@ -0,0 +1,11 @@
+#include <config.h>
+
+int write_config(const char *src)
+{
+
+}
+
+int read_config(char *dst)
+{
+
+}
