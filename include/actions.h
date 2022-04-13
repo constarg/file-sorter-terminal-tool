a0e28a390cab33e9c13fb5fbed8393eb8bc3fb13
Constantinos Argyriou <constarg@pm.me>
2022-04-13 22:10:42 +0300

    Done comment.

--- include/actions.h
+++ include/actions.h
@@ -20,7 +20,7 @@ extern void set_parse_interval(const struct tool_builder_args *args);
 
 /**
  * Change the default dir path, where the
- * files sented when no rule has been set.
+ * files sent when no rule has been set.
  * @param args The information that the builder pass to the callback.
  */
 extern void set_default_dir_path(const struct tool_builder_args *args);
@@ -28,7 +28,7 @@ extern void set_default_dir_path(const struct tool_builder_args *args);
 /**
  * Enable or disable the default dir
  * feature. This works by changing
- * the value of the coresponded value
+ * the value of the corresponded value
  * in config file.
  * @param args The information that the builder pass to the callback.
  */
@@ -37,7 +37,7 @@ extern void set_enable_default_dir(const struct tool_builder_args *args);
 /**
  * Enable or disable the debug log
  * feature. This works by changing
- * the value of the coresponded value
+ * the value of the corresponded value
  * in config file.
  * @param args The information that the builder pass to the callback.
  */
@@ -53,26 +53,62 @@ extern void add_check(const struct tool_builder_args *args);
 
 /**
  * Add a new rule on the target list that the core
- * checks in order to orginize the files.
+ * checks in order to organize the files.
  * @param args The information that the builder pass to the callback.
  */
 extern void add_target(const struct tool_builder_args *args);
 
+/**
+ * Add a new rule on the exlude list that the core
+ * checks in order to exclude files.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void add_exclude(const struct tool_builder_args *args);
 
+/**
+ * Remove a path from the check list that the core
+ * will scan for files.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void remove_check(const struct tool_builder_args *args);
 
+/**
+ * Remove a rule from the target list that the core
+ * checks in order to organize the files.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void remove_target(const struct tool_builder_args *args);
 
+/**
+ * Remove a rule from the exlude list that the core
+ * checks in order to exclude files.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void remove_exclude(const struct tool_builder_args *args);
 
-// Show commmands.
+// Show commands.
+/**
+ * Show the available options.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void list_options(const struct tool_builder_args *args);
 
+/**
+ * Show the list of path's in check list.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void list_checks(const struct tool_builder_args *args);
 
+/**
+ * Show the list of rules in target list.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void list_targets(const struct tool_builder_args *args);
 
+/**
+ * Show the list of rules in exclude list.
+ * @param args The information that the builder pass to the callback.
+ */
 extern void list_excludes(const struct tool_builder_args *args);
 
 #endif
