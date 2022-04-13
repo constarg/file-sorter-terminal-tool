60dce2b9d7aac20f8ad8945b15fbfcfea32ae207
Constantinos Argyriou <rounnus@gmail.com>
2022-04-13 14:44:11 +0300

    Add commands.

--- main.c
+++ main.c
@@ -1,32 +1,80 @@
 #include <tool_builder/tool_builder.h>
 
+
+
 static void configure_option_commands(struct tool_builder *t_builder)
 {
-        tool_builder_add_both(
-                t_builder,
-                "--set-check-interval",
-                1,
-                NULL,
-                "[value] Change the value of check interval."
+        tool_builder_add_both(t_builder,"--set-check-interval",1,NULL,
+                " [value]\t  Change the value of check interval."
         );
+
+        tool_builder_add_both(t_builder,"--set-parse-interval",1,NULL,
+                " [value]\t  Change the value of parse interval."
+        );
+
+        tool_builder_add_both(t_builder,"--set-default-dir-path",1,NULL,
+                " [path]\t  Change the default directory path."
+        );
+
+        tool_builder_add_both(t_builder,"--set-enable-default-dir",1,NULL,
+                " [value]  0:1 Enable the to transfer files in default dir."
+        );
+
+        tool_builder_add_both(t_builder,"--set-debug-log",1,NULL,
+                " [value]\t\t  0:1 Change the log to debug mode (1)."
+        );
+
 }
 
 static void configure_list_commands(struct tool_builder *t_builder)
 {
+        tool_builder_add_both(t_builder,"--add-check",1,NULL,
+                " [path]\t\t  Add new check."
+        );
+        tool_builder_add_both(
+                t_builder,"--add-target",1,NULL,
+                " [ext] [path]\t  Add new target."
+        );
 
+        tool_builder_add_both(t_builder,"--remove-check",1,NULL,
+                " [row number]\t  remove check."
+        );
+
+        tool_builder_add_both(t_builder,"--remove-target",1,NULL,
+                " [row number]\t  remove target."
+        );
+}
+
+static void configure_show_commands(struct tool_builder *t_builder)
+{
+        tool_builder_add_both(t_builder,"--list-checks",1,NULL,
+                "\t\t\t  list checks."
+        );
+
+        tool_builder_add_both(t_builder,"--list-targets",1,NULL,
+                "\t\t\t  list targets."
+        );
+
+        tool_builder_add_both(t_builder,"--list-excludes",1,NULL,
+                "\t\t\t  list excludes."
+        );
+
+        tool_builder_add_both(t_builder,"--list-options",1,NULL,
+                "\t\t\t  list options."
+        );
 }
 
 static void configure_commands(struct tool_builder *t_builder)
 {
         configure_option_commands(t_builder);
         configure_list_commands(t_builder);
+        configure_show_commands(t_builder);
 }
 
 
 int main(int argc, char *argv[])
 {
 	struct tool_builder t_builder;
-        memset(&t_builder, 0x0, sizeof(struct tool_builder));
 	tool_builder_init(&t_builder);
         tool_builder_init_help(&t_builder, "sorter");
 
