c29cde60c9e557365ef9a4e3bea4702d154869bb
Constantinos Argyriou <constarg@pm.me>
2022-04-13 20:00:34 +0300

    add functions that represent each command.

---
+++ src/actions.c
@@ -0,0 +1,77 @@
+#include <actions.h>
+
+
+extern void set_check_interval(const struct tool_builder_args *args)
+{
+
+}
+
+extern void set_parse_interval(const struct tool_builder_args *args)
+{
+
+}
+
+extern void set_default_dir_path(const struct tool_builder_args *args)
+{
+
+}
+
+extern void set_enable_default_dir(const struct tool_builder_args *args)
+{
+
+}
+
+extern void set_debug_log(const struct tool_builder_args *args)
+{
+
+}
+
+extern void add_check(const struct tool_builder_args *args)
+{
+
+}
+
+extern void add_target(const struct tool_builder_args *args)
+{
+
+}
+
+extern void add_exclude(const struct tool_builder_args *args)
+{
+
+}
+
+extern void remove_check(const struct tool_builder_args *args)
+{
+
+}
+
+extern void remove_target(const struct tool_builder_args *args)
+{
+
+}
+
+extern void remove_exclude(const struct tool_builder_args *args)
+{
+
+}
+
+extern void list_options(const struct tool_builder_args *args)
+{
+
+}
+
+extern void list_checks(const struct tool_builder_args *args)
+{
+
+}
+
+extern void list_targets(const struct tool_builder_args *args)
+{
+
+}
+
+extern void list_excludes(const struct tool_builder_args *args)
+{
+
+}
