699949b5495f3272c19da4dfbe23fdca298a838e
Constantinos Argyriou <constarg@pm.me>
2022-11-17 03:20:11 +0200

    Make the tool to recognize each argument

--- src/actions.c
+++ src/actions.c
@@ -1,77 +1,77 @@
 #include <actions.h>
+#include <stdio.h>
 
-
-void set_check_interval(const struct tool_builder_args *args)
+void set_check_interval(const char *n_value)
 {
-
+  printf("set check\n");
 }
 
-void set_parse_interval(const struct tool_builder_args *args)
+void set_parse_interval(const char *n_value)
 {
-
+  printf("set parse\n");
 }
 
-void set_default_dir_path(const struct tool_builder_args *args)
+void set_default_dir_path(const char *n_path)
 {
-
+  printf("set default\n");
 }
 
-void set_enable_default_dir(const struct tool_builder_args *args)
+void set_enable_default_dir(const char *n_state)
 {
-
+  printf("set enable\n");
 }
 
-void set_debug_log(const struct tool_builder_args *args)
+void set_debug_log(const char *n_state)
 {
-
+  printf("set debug\n");
 }
 
-void add_check(const struct tool_builder_args *args)
+void add_check(const char *path)
 {
-
+  printf("add check\n");
 }
 
-void add_target(const struct tool_builder_args *args)
+void add_target(const char *ext, const char *path)
 {
-
+  printf("add target\n");
 }
 
-void add_exclude(const struct tool_builder_args *args)
+void add_exclude(const char *ext, const char *path)
 {
-
+  printf("add exclude\n");
 }
 
-void remove_check(const struct tool_builder_args *args)
+void remove_check(const char *row)
 {
-
+  printf("remove check\n");
 }
 
-void remove_target(const struct tool_builder_args *args)
+void remove_target(const char *row)
 {
-
+  printf("remove_target");
 }
 
-void remove_exclude(const struct tool_builder_args *args)
+void remove_exclude(const char *row)
 {
-
+  printf("remove exclude");
 }
 
-void list_options(const struct tool_builder_args *args)
+void list_options()
 {
-
+  printf("list opt");
 }
 
-void list_checks(const struct tool_builder_args *args)
+void list_checks()
 {
-
+  printf("list check");
+}
+  
+void list_targets()
+{
+  printf("list targets");
 }
 
-void list_targets(const struct tool_builder_args *args)
+void list_excludes()
 {
-
-}
-
-void list_excludes(const struct tool_builder_args *args)
-{
-
+  printf("list excludes");
 }
