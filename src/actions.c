9345489ab64dc9ef18eda14f281683f643fafebf
Constantinos Argyriou <argyrioukost@gmail.com>
2022-11-17 03:32:14 +0200

    Add help message

--- src/actions.c
+++ src/actions.c
@@ -1,77 +1,77 @@
 #include <actions.h>
 #include <stdio.h>
 
-void set_check_interval(const char *n_value)
+int set_check_interval(const char *n_value)
 {
   printf("set check\n");
 }
 
-void set_parse_interval(const char *n_value)
+int set_parse_interval(const char *n_value)
 {
   printf("set parse\n");
 }
 
-void set_default_dir_path(const char *n_path)
+int set_default_dir_path(const char *n_path)
 {
   printf("set default\n");
 }
 
-void set_enable_default_dir(const char *n_state)
+int set_enable_default_dir(const char *n_state)
 {
   printf("set enable\n");
 }
 
-void set_debug_log(const char *n_state)
+int set_debug_log(const char *n_state)
 {
   printf("set debug\n");
 }
 
-void add_check(const char *path)
+int add_check(const char *path)
 {
   printf("add check\n");
 }
 
-void add_target(const char *ext, const char *path)
+int add_target(const char *ext, const char *path)
 {
   printf("add target\n");
 }
 
-void add_exclude(const char *ext, const char *path)
+int add_exclude(const char *ext, const char *path)
 {
   printf("add exclude\n");
 }
 
-void remove_check(const char *row)
+int remove_check(const char *row)
 {
   printf("remove check\n");
 }
 
-void remove_target(const char *row)
+int remove_target(const char *row)
 {
   printf("remove_target");
 }
 
-void remove_exclude(const char *row)
+int remove_exclude(const char *row)
 {
   printf("remove exclude");
 }
 
-void list_options()
+int list_options()
 {
   printf("list opt");
 }
 
-void list_checks()
+int list_checks()
 {
   printf("list check");
 }
   
-void list_targets()
+int list_targets()
 {
   printf("list targets");
 }
 
-void list_excludes()
+int list_excludes()
 {
   printf("list excludes");
 }
