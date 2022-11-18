b728f21ebff13fed13e34f708e20d74bef1899e8
Constantinos Argyriou <argyrioukost@gmail.com>
2022-11-18 22:33:45 +0200

    add open and close

--- src/config.c
+++ src/config.c
@@ -25,7 +25,7 @@ static inline int get_conf_path(char **dst)
 
 int write_config(const struct conf *src)
 {
-    char *conf_path = NULL;
+    /*char *conf_path = NULL;
     size_t size_src = strlen(src);
     FILE *conf = NULL;
 
@@ -41,14 +41,33 @@ int write_config(const struct conf *src)
         fclose(conf);
         return -1;
     }
-    fclose(conf);
+    fclose(conf);*/
     return 0;
 }
 
-int read_config(struct conf *dst)
+/**
+ *  This function convert the given text file
+ *  to a conf structure.
+ *  @param dst The config.
+ *  @param text The text file to convert from.
+ */
+static int conv_to_conf(struct conf *dst,
+                        const char *text)
+{
+    char *text_dup = strdup(text);
+    // retrieve options.
+
+
+
+
+    free(text_dup);
+}
+
+int open_config(struct conf *dst)
 {
     char *conf_path = NULL;
     struct stat stat_buff;
+    char *conf_content = NULL;
     FILE *conf = NULL;
 
     // get the absolute path.
@@ -63,12 +82,12 @@ int read_config(struct conf *dst)
     free(conf_path);
 
     // allocate space for the dst.
-    (*dst) = (char *) malloc(sizeof(char) *
-                             stat_buff.st_size + 1);
-    if (*dst == NULL) return -1;
+    conf_content = (char *) malloc(sizeof(char) *
+                                   stat_buff.st_size + 1);
+    if (conf_content == NULL) return -1;
 
     // read the content of config file.
-    if (fread(*dst, stat_buff.st_size,
+    if (fread(conf_content, stat_buff.st_size,
               1, conf) != stat_buff.st_size) {
         fclose(conf);
         return -1;
@@ -76,3 +95,35 @@ int read_config(struct conf *dst)
     fclose(conf);
     return 0;
 }
+
+void close_config(struct conf *src)
+{
+    // free options.
+    free(src->c_options.c_check_intrv);
+    free(src->c_options.c_parse_intrv);
+    free(src->c_options.c_debug_log);
+    free(src->c_options.c_default_dir_p);
+    free(src->c_options.c_enable_p);
+
+    // close lists.
+    // check list.
+    for (int c = 0; 
+         src->c_lists.c_check_list[c]; c++) {
+        free(src->c_lists.c_check_list[c]);
+    }
+    // target list.
+    for (int t = 0;
+         src->c_lists.c_target_list[t]; t++) {
+        free(src->c_lists.c_target_list[t]); 
+    }
+    // exclude list.
+    for (int e = 0;
+         src->c_lists.c_exclude_list[e]; e++) {
+        free(src->c_lists.c_exclude_list[e]);
+    }
+    // clean up last parts
+    free(src->c_lists.c_check_list);
+    free(src->c_lists.c_target_list);
+    free(src->c_lists.c_exclude_list);
+}
+
