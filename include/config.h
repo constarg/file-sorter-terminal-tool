7437270d35d21bc3fbbcb34363780de5728b085b
Constantinos Argyriou <constarg@pm.me>
2022-11-19 01:46:42 +0200

    get the config parser from core

--- include/config.h
+++ include/config.h
@@ -1,67 +1,55 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+/* config.h  */
+#ifndef FILE_SORTER_CORE_CONFIG_H
+#define FILE_SORTER_CORE_CONFIG_H
 
 #include <memory.h>
 
+#define FAILED_TO_PARSE	-0x1
+
+
+// config options.
+struct options
+{
+	unsigned int o_check_interval;		// check interval option.
+	unsigned int o_parse_interval;		// parse interval option.
+	unsigned int o_debug_log:1;		    // debug log option.
+	char        *o_default_path;		// default path option.
+	unsigned int o_enable_default:1;	// enable default path option.
+	unsigned int o_move_no_ext:1;		// move files that has no extention.
+};
+
+// config lists.
+struct lists
+{
+	char *(*l_check_list);			    // the check list of the config.
+	char *(*l_target_list);			    // the targests of the config.
+	char *(*l_exclude_list);		    // the excludes of the config.
+};
+
+// the config file.
+struct config
+{
+	struct options c_options; 		// the options of the config.
+	struct lists c_lists;			// the lists of the config.
+};
+
+
 /**
- * ------------------
- *  Config Options
- * ------------------
- */
-struct conf_opt 
+	Initialize the variable that contains the config
+	file.
+**/
+static void inline init_config(struct config *config)
 {
-    char *c_check_intrv;      // Check interval.
-    char *c_parse_intrv;      // Parse interval.
-    char *c_debug_log;         // Debug log.
-    char *c_default_dir_p;    // default directory path.
-    char *c_enable_p;         // enable default directory.
-};
-
-/**
- * ----------------
- *  Config Lists
- * ----------------
- */
-struct conf_lists
-{
-    char *(*c_check_list);      // Check list.
-    char *(*c_target_list);     // Target list.
-    char *(*c_exclude_list);    // Exclude list.
-};
-
-struct conf 
-{
-    struct conf_opt c_options; // The options of the config.
-    struct conf_lists c_lists; // The lists of the config.
-};
-
-
-static inline void init_conf(struct conf *src)
-{
-    memset(src, 0x0, sizeof(struct conf));
+	memset(config, 0x0, sizeof(struct config));
 }
 
-
 /**
- * Write the config file contents.
- * @param src The buffer that contains the changed content.
- * @return 0 on success or -1 on error.
- */
-extern int write_config(const struct conf *src);
+	Parse the config file and save the informations	
+	inside the dst variable.
+	@param dst The destination of where we put the config.
+*/
+extern void parse_config(struct config *dst);
 
-/**
- * Reads the config file contents and
- * store them in a buffer.
- * @param dst The destination buffer.
- * @return 0 on success or -1 on error.
- */
-extern int open_config(struct conf *dst);
-
-/**
- * Close the config file, that was opened 
- * using open_config.
- * @param src The config file.
- */
-extern void close_config(struct conf *src);
+extern void destroy_config(struct config *src);
 
 #endif
