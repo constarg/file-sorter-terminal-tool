4f06f3f826328d96f3e100941b717c2651a4afd2
Constantinos Argyriou <argyrioukost@gmail.com>
2022-11-19 01:46:42 +0200

    get the config parser from core

--- src/config.c
+++ src/config.c
@@ -1,129 +1,151 @@
-#include <config.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <string.h>
-#include <malloc.h>
-#include <stdio.h>
+#include <fcntl.h>
 
-static inline int get_conf_path(char **dst)
+#include "config.h"
+
+#define CONF_PATH "/.local/share/file_sorter/config/config.conf"
+
+
+
+static char *get_config_buff()
 {
-    char *usr = getlogin();
-    if (usr == NULL) return -1;
-    char rel_path[] = ".local/share/file_sorter"
-                      "/config/config.conf";
-    (*dst) = (char *) malloc(sizeof(char) * strlen(usr)
-                                          + strlen(rel_path)
-                                          + 7); // len of /home/ + \0
-    if (*dst == NULL) return -1;
-    // build the absolute path.
-    strcpy(*dst, "/home/");
-    strcat(*dst, usr);
-    strcat(*dst, rel_path);
-    return 0;
+		// determine the absolute path of the config.
+		char *username = getlogin();
+		char *absolute = (char *) malloc(sizeof(char) * (strlen(CONF_PATH) + 
+																										 strlen(username)  + 7));
+		if (absolute == NULL) return NULL;
+
+		strcpy(absolute, "/home/");
+		strcat(absolute, username);
+		strcat(absolute, CONF_PATH);	
+	
+		// read the size of the config.
+		struct stat conf_stat;
+		if (lstat(absolute, &conf_stat) != 0) return NULL; // TODO: Call the logger here.
+	
+		char *conf_buff = (char *) malloc((conf_stat.st_size + 1) * sizeof(char));
+		if (conf_buff == NULL) return NULL;
+	
+		int conf_fd = open(absolute, O_RDONLY);
+		if (conf_fd == -1)  return NULL; 	
+
+		if (read(conf_fd, conf_buff, conf_stat.st_size) == -1) return NULL; // TODO: Call the logger here.
+		close(conf_fd);
+		conf_buff[conf_stat.st_size] = '\0';
+	
+		free(absolute);
+		
+		return conf_buff;
 }
 
-int write_config(const struct conf *src)
+static char *isolate_opt(const char *conf_buff, const char *opt)
 {
-    /*char *conf_path = NULL;
-    size_t size_src = strlen(src);
-    FILE *conf = NULL;
+		// don't change the original buffer.
+		char *tmp = strdup(conf_buff);
+		char *opt_c = strstr(tmp, opt);
+		opt_c = strtok(opt_c, " "); // separete the option and the label.
+		opt_c = strtok(NULL, "\n"); // get the option.
+		opt_c = strdup(opt_c);
 
-    // get the absolute path.
-    if (get_conf_path(&conf_path) == -1) return -1;
-    conf = fopen(conf_path, "w");
-    free(conf_path);
-
-    if (conf == NULL) return -1;
-    // use fwrite, as it is faster than fprintf.
-    if (fwrite(src, size_src, 1, conf)
-        != size_src) {
-        fclose(conf);
-        return -1;
-    }
-    fclose(conf);*/
-    return 0;
+		free(tmp);
+		return opt_c; // return value must be freed.
 }
 
-/**
- *  This function convert the given text file
- *  to a conf structure.
- *  @param dst The config.
- *  @param text The text file to convert from.
- */
-static int conv_to_conf(struct conf *dst,
-                        const char *text)
+static inline unsigned int parse_int_opt(const char *conf_buff, const char *opt)
 {
-    char *text_dup = strdup(text);
-    // retrieve options.
+		char *opt_a = isolate_opt(conf_buff, opt);
+		unsigned int opt_r = (opt_a == NULL)? 0:atoi(opt_a);
+		if (opt_r == 0) return -1;	
 
-
-
-
-    free(text_dup);
+		free(opt_a); 	
+		return opt_r; // get the int value.
 }
 
-int open_config(struct conf *dst)
+static inline char *parse_str_opt(const char *conf_buff, const char *opt)
 {
-    char *conf_path = NULL;
-    struct stat stat_buff;
-    char *conf_content = NULL;
-    FILE *conf = NULL;
-
-    // get the absolute path.
-    if (get_conf_path(&conf_path) == -1) return -1;
-    conf = fopen(conf_path, "r");
-
-    // get stats of the conf file.
-    if (stat(conf_path, &stat_buff) == -1) {
-        free(conf_path);
-        return -1;
-    }
-    free(conf_path);
-
-    // allocate space for the dst.
-    conf_content = (char *) malloc(sizeof(char) *
-                                   stat_buff.st_size + 1);
-    if (conf_content == NULL) return -1;
-
-    // read the content of config file.
-    if (fread(conf_content, stat_buff.st_size,
-              1, conf) != stat_buff.st_size) {
-        fclose(conf);
-        return -1;
-    }    
-    fclose(conf);
-    return 0;
+		return isolate_opt(conf_buff, opt); // get the str value.
 }
 
-void close_config(struct conf *src)
+static char **parse_list(const char *conf_buff, const char *list)
 {
-    // free options.
-    free(src->c_options.c_check_intrv);
-    free(src->c_options.c_parse_intrv);
-    free(src->c_options.c_debug_log);
-    free(src->c_options.c_default_dir_p);
-    free(src->c_options.c_enable_p);
+		// locate the list.
+		char *tmp = strdup(conf_buff);
+		char *loc = strstr(tmp, list);
+		if (loc == NULL) return NULL;
+		char *curr_el = strtok(loc, "\n");
+	
+		unsigned int list_l = 10;
+		unsigned int real_l = 0;
+		char *(*list_r) = (char **) malloc(list_l * sizeof(char *));
+		if (list_r == NULL) return NULL;
+	
+		for (int c = 0; curr_el; c++) {
+			curr_el = strtok(NULL, "\n");
+			if (curr_el == NULL) break;
+			if (!strcmp(curr_el, "[done]")) break;
+			real_l = c + 1;
+	
+			if (real_l == list_l) {	
+				list_l += 10;
+				list_r = (char **) realloc(list_r,  list_l * sizeof(char *));
+				if (list_r == NULL) {
+					return NULL;  	
+				}
+			}
+			list_r[c] = strdup(curr_el);
+		}
+		if (real_l == 0) {
+			free(list_r);
+			free(tmp);
+			return NULL;
+		}
+		
+		// null terminate the array.
+		list_r[real_l] = NULL;
+		free(tmp);
+		return (char **) realloc(list_r, (real_l + 1) * sizeof(char *)); // adjust the size of the list.
+}
 
-    // close lists.
-    // check list.
-    for (int c = 0; 
-         src->c_lists.c_check_list[c]; c++) {
-        free(src->c_lists.c_check_list[c]);
-    }
-    // target list.
-    for (int t = 0;
-         src->c_lists.c_target_list[t]; t++) {
-        free(src->c_lists.c_target_list[t]); 
-    }
-    // exclude list.
-    for (int e = 0;
-         src->c_lists.c_exclude_list[e]; e++) {
-        free(src->c_lists.c_exclude_list[e]);
-    }
-    // clean up last parts
-    free(src->c_lists.c_check_list);
-    free(src->c_lists.c_target_list);
-    free(src->c_lists.c_exclude_list);
+static inline void free_list(char *(**list))
+{
+		if ((*list) == NULL) return;
+		for (int i = 0; (*list)[i]; i++) {
+			free((*list)[i]);
+		}
+		free(*list); 
+		*list = NULL;
+}
+
+void parse_config(struct config *dst)
+{
+		char *conf_buff = get_config_buff();
+		if (!conf_buff) return;
+	
+		dst->c_options.o_check_interval = parse_int_opt(conf_buff, "check_interval");
+		dst->c_options.o_parse_interval = parse_int_opt(conf_buff, "parse_interval");
+		dst->c_options.o_debug_log 			= parse_int_opt(conf_buff, "debug_log") & 0x1;
+		dst->c_options.o_default_path 	= parse_str_opt(conf_buff, "default_dir_path");
+		dst->c_options.o_enable_default = parse_int_opt(conf_buff, "enable_default_path") & 0x1;
+		dst->c_options.o_move_no_ext 		= parse_int_opt(conf_buff, "move_files_without_extention") & 0x1;
+		dst->c_lists.l_check_list 			= parse_list(conf_buff, "[check]");
+		dst->c_lists.l_target_list 			= parse_list(conf_buff, "[targets]");
+		dst->c_lists.l_exclude_list 		= parse_list(conf_buff, "[exclude]");
+		free(conf_buff);
+}
+
+void destroy_config(struct config *src)
+{
+		// free the allocated space where it should.
+		free(src->c_options.o_default_path);
+		src->c_options.o_default_path = NULL;
+		free_list(&src->c_lists.l_check_list);
+		free_list(&src->c_lists.l_target_list);
+		free_list(&src->c_lists.l_exclude_list);
+
 }
 
