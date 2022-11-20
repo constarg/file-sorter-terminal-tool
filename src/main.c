71d3c411c4d4461a8c3e12d7b29f957efc328361
Constantinos Argyriou <constarg@pm.me>
2022-11-20 20:55:38 +0200

    Update doc and set error messages

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ const char usage[] = "Usage:\n \tsorter [OPTION] ...\n\n"
                      "\t--set-default-dir-path    [path] Change the default directory path.\n"
                      "\t--set-enable-default-dir  [value] 0:1 Enable the to transfer files in default dir.\n"
                      "\t--set-debug-log           [value] 0:1 Change the log to debug mode (1).\n"
+                     "\t--set-mv-without-ext      [value] 0:1 Enable the program to move files without extention.\n"
                      "\t--add-check               [path] Add new check.\n"
                      "\t--add-target              [ext] [path] Add new target.\n"
                      "\t--remove-check            [row number] remove check.\n"
@@ -30,42 +31,86 @@ int main(int argc, char *argv[])
         return help();
     } else if (!strcmp(argv[1], "--set-check-interval")) {
         if (argv[2] == NULL) return help();
-        set_check_interval(argv[2]);
+        if (set_check_interval(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--set-parse-interval")) {
         if (argv[2] == NULL) return help();
-        set_parse_interval(argv[2]);
+        if (set_parse_interval(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--set-default-dir-path")) {
         if (argv[2] == NULL) return help();
         set_default_dir_path(argv[2]);
     } else if (!strcmp(argv[1], "--set-enable-default-dir")) {
         if (argv[2] == NULL) return help();
-        set_enable_default_dir(argv[2]);
+        if (set_enable_default_dir(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--set-debug-log")) {
         if (argv[2] == NULL) return help();
-        set_debug_log(argv[2]);
+        if (set_debug_log(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--set-mv-without-ext")) {
         if (argv[2] == NULL) return help();
-        set_mv_without_ext(argv[2]);
+        if (set_mv_without_ext(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--add-check")) {
         if (argv[2] == NULL) return help();
-        add_check(argv[2]);
+        if (add_check(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--add-target")) {
         if (argv[2] == NULL) return help();
         if (argv[3] == NULL) return help();
-        add_target(argv[2], argv[3]);
+        if (add_target(argv[2], argv[3]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--add-exclude")) {
         if (argv[2] == NULL) return help();
         if (argv[3] == NULL) return help();
-        add_exclude(argv[2], argv[3]);
+        if (add_exclude(argv[2], argv[3]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--remove-check")) {
         if (argv[2] == NULL) return help();
-        remove_check(argv[2]);
+        if (remove_check(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--remove-target")) {
         if (argv[2] == NULL) return help();
-        remove_target(argv[2]);
+        if (remove_target(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--remove-exclude")) {
         if (argv[2] == NULL) return help();
-        remove_exclude(argv[2]);
+        if (remove_exclude(argv[2]) == -1) {
+            printf("Error\n");
+        } else {
+            printf("OK\n");
+        }
     } else if (!strcmp(argv[1], "--list-options")) {
         list_options();
     } else if (!strcmp(argv[1], "--list-checks")) {
